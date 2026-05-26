458812983228ecb1fc1ad0a1a646bb986314fe9e
david <davsperling@gmail.com>
2026-05-26 17:42:51 +0300

    session 4: MCP tavily + filesystem + LangSmith tracing

--- agent_mcp.py
+++ agent_mcp.py
@@ -13,17 +13,27 @@ async def main():
     # MultiServerMCPClient prend un dict : nom_serveur → config
     # config stdio a besoin de 3 clés : command, args, transport
     config = {
-        "filesystem": {
+  
+    "tavily-mcp": {
+      "command": "npx",
+      "transport": "stdio",
+      "args": ["-y", "tavily-mcp@latest"],
+      "env": {
+        "TAVILY_API_KEY": os.environ["TAVILY_API_KEY"],
+        "DEFAULT_PARAMETERS": "{\"include_images\": true, \"max_results\": 15, \"search_depth\": \"advanced\"}"
+      }},
+      "filesystem": {
             "command": "npx",
             "args": ["@modelcontextprotocol/server-filesystem", "/Users/david/Documents/Python/FORMATION_IA/github/agents-playground"],
             "transport": "stdio",
         }
-    }
+  
+}
 
     client = MultiServerMCPClient(config)
     # Récupère la liste des tools exposés par le serveur
     tools = await client.get_tools()
-    print([t.name for t in tools])  # juste pour vérifier
+    #print([t.name for t in tools])  # juste pour vérifier
 
     # TODO : créer l'agent avec ChatOpenAI gpt-4o-mini + ces tools
     model = ChatOpenAI(model="gpt-4o-mini")
@@ -31,7 +41,7 @@ async def main():
     agent =  create_react_agent(model, tools)
 
     # TODO : invoquer avec "List the files in the current directory"
-    result =await  agent.ainvoke({"messages": [("human", "List the files in the current directory")]} )
+    result =await  agent.ainvoke({"messages": [("human", "cherche les actus du jour et écris un résumé dans un fichier résumé.txt")]} )
     print(result)
 
 asyncio.run(main())
